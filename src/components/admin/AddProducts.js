86192d2a1bf7eaf18e34fdcc6d2f01fd72c8bb49
Gokulakrishnan0610 <gokulakilan06@gmail.com>
2024-10-18 14:01:55 +0530

    Add Products Page

--- src/components/admin/AddProducts.js
+++ src/components/admin/AddProducts.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "../admin/AddProducts.css"
 
 const AddProducts = () => {
     const [products, setProducts] = useState([]);
@@ -19,6 +21,16 @@ const AddProducts = () => {
         additionalInfo: ''
     });
     const [file, setFile] = useState(null);
+    const [editMode, setEditMode] = useState(false);
+    const [productIdToEdit, setProductIdToEdit] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/products')
+            .then((response) => setProducts(response.data))
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     const handleInputChange = (e) => {
         const { name, value, type, checked } = e.target;
@@ -67,11 +79,81 @@ const AddProducts = () => {
 
         console.log(productData); // Log productData for debugging
 
-        // Add logic to handle product submission to the backend
+        try {
+            if (editMode) {
+                // Update product
+                const response = await axios.put(`http://localhost:5000/products/${productIdToEdit}`, productData);
+                setProducts((prevProducts) =>
+                    prevProducts.map((product) =>
+                        product._id === productIdToEdit ? response.data : product
+                    )
+                );
+                setEditMode(false);
+                setProductIdToEdit(null);
+            } else {
+                // Create new product
+                const response = await axios.post('http://localhost:5000/products', productData, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                setProducts((prevProducts) => [...prevProducts, response.data]);
+            }
+
+            // Reset the form
+            setForm({
+                SKU: '',
+                name: '',
+                price: '',
+                image: '',
+                gender: '',
+                age: '',
+                size: '',
+                color: '',
+                vaccinated: false,
+                dewormed: false,
+                cert: '',
+                location: '',
+                publishedDate: '',
+                additionalInfo: ''
+            });
+            setFile(null);
+        } catch (error) {
+            console.error('Error adding/updating product:', error);
+        }
+    };
+
+
+    const handleDelete = (id) => {
+        axios.delete(`http://localhost:5000/products/${id}`)
+            .then(() => setProducts((prevProducts) => prevProducts.filter((product) => product._id !== id)))
+            .catch((error) => console.error('Error deleting product:', error));
+    };
+
+    const handleEdit = (product) => {
+        setForm({
+            SKU: product.SKU,
+            name: product.name,
+            price: product.price,
+            image: product.image,
+            gender: product.gender,
+            age: product.age,
+            size: product.size,
+            color: product.color,
+            vaccinated: product.vaccinated,
+            dewormed: product.dewormed,
+            cert: product.cert,
+            location: product.location,
+            additionalInfo: product.additionalInfo
+        });
+        setProductIdToEdit(product._id);
+        setEditMode(true);
     };
 
     return (
-        <div className="overflow-auto max-h-screen p-6 bg-gray-100">
+
+
+        <div className="scroll-container overflow-auto max-h-screen p-6 bg-gray-100 lg:w-full lg:py-10 lg:px-24">
             <form onSubmit={handleSubmit} className="max-w-full w-full overflow-auto bg-white rounded-lg shadow-md p-6">
                 <h2 className="text-xl font-bold mb-4">Add New Product</h2>
 
@@ -79,7 +161,7 @@ const AddProducts = () => {
                     type="text"
                     name="SKU"
                     placeholder="SKU"
-                    value={form.SKU}
+                    value={form.SKU || ''}
                     onChange={handleInputChange}
                     required
                     className="border border-gray-300 p-3 mb-4 w-full rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -87,8 +169,8 @@ const AddProducts = () => {
                 <input
                     type="text"
                     name="name"
-                    placeholder="Product Name"
-                    value={form.name}
+                    placeholder="Name"
+                    value={form.name || ''}
                     onChange={handleInputChange}
                     required
                     className="border border-gray-300 p-3 mb-4 w-full rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -96,8 +178,8 @@ const AddProducts = () => {
                 <input
                     type="number"
                     name="price"
-                    placeholder="Product Price"
-                    value={form.price}
+                    placeholder="Price"
+                    value={form.price || ''}
                     onChange={handleInputChange}
                     required
                     className="border border-gray-300 p-3 mb-4 w-full rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -139,10 +221,10 @@ const AddProducts = () => {
                 </div>
 
                 <input
-                    type="number"
+                    type="text"
                     name="age"
                     placeholder="Age"
-                    value={form.age}
+                    value={form.age || ''}
                     onChange={handleInputChange}
                     className="border border-gray-300 p-3 mb-4 w-full rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
@@ -150,7 +232,7 @@ const AddProducts = () => {
                     type="text"
                     name="size"
                     placeholder="Size"
-                    value={form.size}
+                    value={form.size || ''}
                     onChange={handleInputChange}
                     required
                     className="border border-gray-300 p-3 mb-4 w-full rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -159,7 +241,7 @@ const AddProducts = () => {
                     type="text"
                     name="color"
                     placeholder="Color"
-                    value={form.color}
+                    value={form.color || ''}
                     onChange={handleInputChange}
                     required
                     className="border border-gray-300 p-3 mb-4 w-full rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -194,8 +276,8 @@ const AddProducts = () => {
                 <input
                     type="text"
                     name="cert"
-                    placeholder="Certification"
-                    value={form.cert}
+                    placeholder="Cert"
+                    value={form.cert || ''}
                     onChange={handleInputChange}
                     required
                     className="border border-gray-300 p-3 mb-4 w-full rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -204,7 +286,7 @@ const AddProducts = () => {
                     type="text"
                     name="location"
                     placeholder="Location"
-                    value={form.location}
+                    value={form.location || ''}
                     onChange={handleInputChange}
                     required
                     className="border border-gray-300 p-3 mb-4 w-full rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -212,7 +294,7 @@ const AddProducts = () => {
                 <input
                     type="date"
                     name="publishedDate"
-                    value={form.publishedDate}
+                    value={form.publishedDate || ''}
                     onChange={handleInputChange}
                     required
                     className="border border-gray-300 p-3 mb-4 w-full rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -220,7 +302,7 @@ const AddProducts = () => {
                 <textarea
                     name="additionalInfo"
                     placeholder="Additional Information"
-                    value={form.additionalInfo}
+                    value={form.additionalInfo || ''}
                     onChange={handleInputChange}
                     className="border border-gray-300 p-3 mb-4 w-full rounded focus:outline-none focus:ring-2 focus:ring-blue-500 h-24"
                 />
@@ -228,6 +310,39 @@ const AddProducts = () => {
                     Add Product
                 </button>
             </form>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-20">
+                {products.map((product) => (
+                    <div key={product._id} className="border p-4 rounded bg-white">
+                        <img src={product.image} alt={product.name} className="w-full h-64 object-cover mb-4 rounded" />
+                        <h3 className="text-2xl font-semibold">{product.name}</h3>
+                        <p className="text-base font-semibold">Rs. {product.price}</p>
+                        <p>SKU: {product.SKU}</p>
+                        <p>Gender: {product.gender}</p>
+                        <p>Age: {product.age}</p>
+                        <p>Size: {product.size}</p>
+                        <p>Color: {product.color}</p>
+                        <p>Vaccinated: {product.vaccinated ? 'Yes' : 'No'}</p>
+                        <p>Dewormed: {product.dewormed ? 'Yes' : 'No'}</p>
+                        <p>Cert: {product.cert}</p>
+                        <p>Location: {product.location}</p>
+                        <p>Published Date: {product.publishedDate}</p>
+                        <p>Additional Information: {product.additionalInfo}</p>
+
+                        <button
+                            onClick={() => handleDelete(product._id)}
+                            className="bg-red-500 text-white px-4 py-2 mt-2 mr-2"
+                        >
+                            Delete
+                        </button>
+                        <button
+                            onClick={() => handleEdit(product)}
+                            className="bg-yellow-500 text-white px-4 py-2 mt-2"
+                        >
+                            Edit
+                        </button>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
